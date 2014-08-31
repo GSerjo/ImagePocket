d234677576ef8ea8bd834d4346dc5e2c70eba928
GSerjo <sedot8@gmail.com>
2014-08-31 14:35:33 +0400

    + ColletionViewWaterfallLayout initial version

---
+++ Source/Dojo/Layouts/CollectionViewWaterfallLayout.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using MonoTouch.UIKit;
+using System.Collections.Generic;
+
+namespace Dojo
+{
+	public sealed class CollectionViewWaterfallLayout : UICollectionViewLayout
+	{
+		private const string CHTCollectionElementKindSectionHeader = "CHTCollectionElementKindSectionHeader";
+		private const string CHTCollectionElementKindSectionFooter = "CHTCollectionElementKindSectionFooter";
+		private int _columnCount;
+		private float _minimumColumnSpacing;
+		private float _minimumInteritemSpacing;
+		private float _headerHeight;
+		private float _footerHeight;
+		private UIEdgeInsets _sectionInset;
+		private List<float> _columnHeights = new List<float>();
+
+		public CollectionViewWaterfallLayout ()
+		{
+			HeaderHeight = 0;
+			FooterHeight = 0;
+			ColumnCount = 2;
+			MinimumInteritemSpacing = 10;
+			MinimumColumnSpacing = 10;
+			SectionInset = UIEdgeInsets.Zero;
+		}
+
+		public int ColumnCount
+		{
+			get { return _columnCount; }
+			set
+			{
+				_columnCount = value;
+				InvalidateLayout ();
+			}
+		}
+
+		public float MinimumColumnSpacing
+		{
+			get { return _minimumColumnSpacing; }
+			set
+			{
+				_minimumColumnSpacing = value;
+				InvalidateLayout ();
+			}
+		}
+
+		public float MinimumInteritemSpacing
+		{
+			get { return _minimumInteritemSpacing; }
+			set
+			{
+				_minimumInteritemSpacing = value;
+				InvalidateLayout ();
+			}
+		}
+
+		public float HeaderHeight
+		{
+			get { return _headerHeight; }
+			set
+			{
+				_headerHeight = value;
+				InvalidateLayout ();
+			}
+		}
+
+		public float FooterHeight
+		{
+			get { return _footerHeight; }
+			set
+			{
+				_footerHeight = value;
+				InvalidateLayout ();
+			}
+		}
+
+		public UIEdgeInsets SectionInset
+		{
+			get { return _sectionInset; }
+			set
+			{
+				_sectionInset = value;
+				InvalidateLayout ();
+			}
+		}
+	}
+}
+
---
+++ Source/Dojo/Layouts/CollectionViewWaterfallLayoutItemRenderDirection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Dojo
+{
+	public enum CollectionViewWaterfallLayoutItemRenderDirection
+	{
+		ShortestFirst,
+		LeftToRight,
+		RightToLeft
+	}
+}
