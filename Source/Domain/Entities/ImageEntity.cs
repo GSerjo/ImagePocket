539adcde5ed3d4a9404fe31de78f3164d01cb69b
GSerjo <sedot8@gmail.com>
2015-05-01 15:56:20 +0300

    * All clean code

--- Source/Domain/Entities/ImageEntity.cs
+++ Source/Domain/Entities/ImageEntity.cs
@@ -1,131 +1,128 @@
 ﻿using System;
-using SQLite;
 using System.Collections.Generic;
 using System.Linq;
 using Core;
+using SQLite;
 
 namespace Domain
 {
-	public sealed class ImageEntity : Entity
-	{
-		private List<TagEntity> _tags = new List<TagEntity>();
-		private const string Separator = ",";
-		private TagCache _tagRepository = TagCache.Instance;
-		private string _tagsInternal;
+    public sealed class ImageEntity : Entity
+    {
+        private const string Separator = ",";
+        private readonly TagCache _tagRepository = TagCache.Instance;
+        private List<TagEntity> _tags = new List<TagEntity>();
+        private string _tagsInternal;
 
-		public ImageEntity ()
-		{
-			CreateTime = DateTime.MinValue;
-		}
+        public ImageEntity()
+        {
+            CreateTime = DateTime.MinValue;
+        }
 
-		[Indexed]
-		public string LocalIdentifier { get; set; }
+        public DateTime CreateTime { get; set; }
 
-		public string TagsInternal
-		{
-			get
-			{
-				_tagsInternal = TagsToString ();
-				return _tagsInternal;
-			}
-			set
-			{
-				_tagsInternal = value;
-			}
-		}
+        [Indexed]
+        public string LocalIdentifier { get; set; }
 
-		public DateTime CreateTime { get; set; }
+        [Ignore]
+        public List<TagEntity> Tags
+        {
+            get
+            {
+                if (_tags.IsNotEmpty())
+                {
+                    return _tags;
+                }
+                if (string.IsNullOrEmpty(_tagsInternal))
+                {
+                    return new List<TagEntity>();
+                }
+                _tags = _tagsInternal.Split(Separator[0])
+                                     .Select(x => int.Parse(x))
+                                     .Select(x => _tagRepository.GetById(x))
+                                     .ToList();
+                return _tags;
+            }
+        }
 
-		[Ignore]
-		public List<TagEntity> Tags
-		{
-			get
-			{
-				if (_tags.IsNotEmpty())
-				{
-					return _tags;
-				}
-				if (string.IsNullOrEmpty (_tagsInternal)) 
-				{
-					return new List<TagEntity> ();
-				}
-				_tags = _tagsInternal.Split(Separator[0])
-					.Select (x => int.Parse (x))
-					.Select(x => _tagRepository.GetById(x))
-					.ToList ();
-				return _tags;
-			}
-		}
+        public string TagsInternal
+        {
+            get
+            {
+                _tagsInternal = TagsToString();
+                return _tagsInternal;
+            }
+            set { _tagsInternal = value; }
+        }
 
-		public List<TagEntity> GetRemovedTags(ImageEntity entity)
-		{
-			var result = Tags.Except (entity.Tags, new FuncComparer<TagEntity> ((x, y) => x.Equals(y)));
-			return result.ToList();
-		}
+        public void AddTag(TagEntity tag)
+        {
+            if (TagExists(tag))
+            {
+                return;
+            }
+            _tags.Add(tag);
+            _tagsInternal = TagsToString();
+        }
 
-		public ImageEntity CloneDeep()
-		{
-			var result = new ImageEntity
-			{
-				EntityId = EntityId,
-				LocalIdentifier = LocalIdentifier,
-				TagsInternal = TagsInternal,
-				CreateTime = CreateTime
-			};
-			return result;
-		}
+        public ImageEntity CloneDeep()
+        {
+            var result = new ImageEntity
+            {
+                EntityId = EntityId,
+                LocalIdentifier = LocalIdentifier,
+                TagsInternal = TagsInternal,
+                CreateTime = CreateTime
+            };
+            return result;
+        }
 
-		public void AddTag(TagEntity tag)
-		{
-			if (TagExists(tag))
-			{
-				return;
-			}
-			_tags.Add (tag);
-			_tagsInternal = TagsToString ();
-		}
+        public bool ContainsTag(TagEntity tag)
+        {
+            return TagExists(tag);
+        }
 
-		public void RemoveTag (TagEntity tag)
-		{
-			_tags.RemoveAll(x => x.EntityId == tag.EntityId);
-			_tagsInternal = TagsToString ();
-		}
+        public bool Equals(ImageEntity value)
+        {
+            if (ReferenceEquals(null, value))
+            {
+                return false;
+            }
+            if (ReferenceEquals(this, value))
+            {
+                return true;
+            }
+            return value.EntityId == EntityId;
+        }
 
-		public bool ContainsTag(TagEntity tag)
-		{
-			return TagExists (tag);
-		}
+        public override int GetHashCode()
+        {
+            return ((LocalIdentifier != null ? LocalIdentifier.GetHashCode() : 0) * 397) ^ EntityId;
+        }
 
-		public override int GetHashCode ()
-		{
-			return ((LocalIdentifier != null ? LocalIdentifier.GetHashCode () : 0) * 397) ^ EntityId;
-		}
+        public List<TagEntity> GetRemovedTags(ImageEntity entity)
+        {
+            IEnumerable<TagEntity> result = Tags.Except(entity.Tags, new FuncComparer<TagEntity>((x, y) => x.Equals(y)));
+            return result.ToList();
+        }
 
-		public bool Equals(ImageEntity value)
-		{
-			if (ReferenceEquals (null, value))
-			{
-				return false;
-			}
-			if(ReferenceEquals(this, value))
-			{
-				return true;
-			}
-			return value.EntityId == EntityId;		
-		}
+        public void RemoveTag(TagEntity tag)
+        {
+            _tags.RemoveAll(x => x.EntityId == tag.EntityId);
+            _tagsInternal = TagsToString();
+        }
 
-		private string TagsToString()
-		{
-			if (_tags.IsNullOrEmpty ())
-			{
-				return string.Empty;
-			}
-			return string.Join (Separator, _tags.Select(x => x.EntityId));
-		}
+        private bool TagExists(TagEntity tag)
+        {
+            return Tags.Exists(x => x.EntityId == tag.EntityId);
+        }
 
-		private bool TagExists(TagEntity tag)
-		{
-			return Tags.Exists (x => x.EntityId == tag.EntityId);
-		}
-	}
+        private string TagsToString()
+        {
+            if (_tags.IsNullOrEmpty())
+            {
+                return string.Empty;
+            }
+            return string.Join(Separator, _tags.Select(x => x.EntityId));
+        }
+    }
 }
