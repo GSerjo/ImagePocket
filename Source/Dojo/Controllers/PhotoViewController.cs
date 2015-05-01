113a4dfae317d8f59ac19b76a8a9636a5445976f
GSerjo <sedot8@gmail.com>
2015-05-01 12:56:05 +0300

    * All clean code

--- Source/Dojo/Controllers/PhotoViewController.cs
+++ Source/Dojo/Controllers/PhotoViewController.cs
@@ -1,67 +1,66 @@
 ﻿using System;
-using MonoTouch.UIKit;
-using MonoTouch.Photos;
-using System.Drawing;
-using Domain;
-using Core;
 using System.Collections.Generic;
 using System.Linq;
+using Core;
+using Domain;
+using MonoTouch.Photos;
+using MonoTouch.UIKit;
 
 namespace Dojo
 {
-	public sealed class PhotoViewController : UIViewController
-	{
-		private readonly PHAsset _asset;
-		private readonly UIBarButtonItem _tabButton;
-		private UIImageView _imageView;
-		private bool _fullScreen;
-		private ImageEntity _image;
-		private readonly ImageCache _imageCache = ImageCache.Instance;
+    public sealed class PhotoViewController : UIViewController
+    {
+        private readonly PHAsset _asset;
+        private readonly ImageCache _imageCache = ImageCache.Instance;
+        private bool _fullScreen;
+        private ImageEntity _image;
+        private UIImageView _imageView;
 
-		public PhotoViewController (ImageEntity image)
-		{
-			Title = "Image";
-			_image = image;
-			_asset = _imageCache.GetAsset (image.LocalIdentifier);
-			_tabButton = new UIBarButtonItem ("Tag", UIBarButtonItemStyle.Plain, OnTagClicked);
-			NavigationItem.RightBarButtonItem = _tabButton;
-		}
+        public PhotoViewController(ImageEntity image)
+        {
+            Title = "Image";
+            _image = image;
+            _asset = _imageCache.GetAsset(image.LocalIdentifier);
+            var tabButton = new UIBarButtonItem("Tag", UIBarButtonItemStyle.Plain, OnTagClicked);
+            NavigationItem.RightBarButtonItem = tabButton;
+        }
 
-		public override void ViewDidLoad ()
-		{
-			View.BackgroundColor = UIColor.White;
-			_imageView = new UIImageView (View.Frame);
-			PHImageManager.DefaultManager.RequestImageForAsset (_asset, View.Frame.Size, 
-				PHImageContentMode.AspectFit, new PHImageRequestOptions (), (img, info) => {
-					_imageView.ContentMode = UIViewContentMode.ScaleAspectFit;
-					_imageView.Image = img;
-			});
-			View.AddSubview (_imageView);
-			var tapGesture = new UITapGestureRecognizer (OnViewTap);
-			View.AddGestureRecognizer (tapGesture);
-		}
+        public override void ViewDidLoad()
+        {
+            View.BackgroundColor = UIColor.White;
+            _imageView = new UIImageView(View.Frame);
+            PHImageManager.DefaultManager.RequestImageForAsset(_asset, View.Frame.Size,
+                PHImageContentMode.AspectFit, new PHImageRequestOptions(), (img, info) =>
+                {
+                    _imageView.ContentMode = UIViewContentMode.ScaleAspectFit;
+                    _imageView.Image = img;
+                });
+            View.AddSubview(_imageView);
+            var tapGesture = new UITapGestureRecognizer(OnViewTap);
+            View.AddGestureRecognizer(tapGesture);
+        }
 
-		private void OnViewTap(UITapGestureRecognizer gesture)
-		{
-			_fullScreen = !_fullScreen;
-			NavigationController.SetNavigationBarHidden (_fullScreen, false);
-			View.BackgroundColor = _fullScreen ? UIColor.Black : UIColor.White;
-		}
+        private void OnTagClicked(object sender, EventArgs ea)
+        {
+            var controller = new TagSelectorViewController(_image)
+            {
+                ModalPresentationStyle = UIModalPresentationStyle.FormSheet
+            };
+            controller.Done += OnTagSelectorDone;
+            NavigationController.PresentViewController(controller, true, null);
+        }
 
-		private void OnTagClicked(object sender, EventArgs ea)
-		{
-			var controller = new TagSelectorViewController (_image)
-			{
-				ModalPresentationStyle = UIModalPresentationStyle.FormSheet
-			};
-			controller.Done += OnTagSelectorDone;
-			NavigationController.PresentViewController (controller, true, null);
-		}
+        private void OnTagSelectorDone(object sender, EventArgsOf<List<ImageEntity>> ea)
+        {
+            _imageCache.SaveOrUpdate(ea.Data);
+            _image = ea.Data.First();
+        }
 
-		private void OnTagSelectorDone(object sender, EventArgsOf<List<ImageEntity>> ea)
-		{
-			_imageCache.SaveOrUpdate (ea.Data);
-			_image = ea.Data.First();
-		}
-	}
+        private void OnViewTap(UITapGestureRecognizer gesture)
+        {
+            _fullScreen = !_fullScreen;
+            NavigationController.SetNavigationBarHidden(_fullScreen, false);
+            View.BackgroundColor = _fullScreen ? UIColor.Black : UIColor.White;
+        }
+    }
 }
--- Source/Dojo/Controllers/TagSelectorViewController.cs
+++ Source/Dojo/Controllers/TagSelectorViewController.cs
@@ -1,262 +1,268 @@
 ﻿using System;
-using System.Drawing;
-using MonoTouch.Foundation;
-using MonoTouch.UIKit;
-using Domain;
 using System.Collections.Generic;
-using MonoTouch.CoreFoundation;
 using System.Linq;
 using Core;
+using Domain;
+using MonoTouch.CoreFoundation;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
 using NSTokenView;
 
 namespace Dojo
 {
-	public partial class TagSelectorViewController : UIViewController
-	{
-		public event EventHandler<EventArgs> Cancel = delegate { };
-		public event EventHandler<EventArgsOf<List<ImageEntity>>> Done = delegate { };
-		private static TagCache _tagRepository = TagCache.Instance;
-		private List<ImageEntity> _images = new List<ImageEntity>();
-		private TagTableSource _tagTableSource;
-		private TagTokenDelegate _tagTokenDelegate;
-		private TagTokenSource _tagTokenSource;
+    public partial class TagSelectorViewController : UIViewController
+    {
+        private static readonly TagCache _tagRepository = TagCache.Instance;
+        private readonly List<ImageEntity> _images = new List<ImageEntity>();
+        private TagTableSource _tagTableSource;
+        private TagTokenDelegate _tagTokenDelegate;
+        private TagTokenSource _tagTokenSource;
 
-		public TagSelectorViewController (ImageEntity image) : base ("TagSelectorViewController", null)
-		{
-			_images.Add (image.CloneDeep());
-		}
+        public TagSelectorViewController(ImageEntity image) : base("TagSelectorViewController", null)
+        {
+            _images.Add(image.CloneDeep());
+        }
 
-		public TagSelectorViewController (List<ImageEntity> images) : base ("TagSelectorViewController", null)
-		{
-			_images.AddRange (images.Select (x => x.CloneDeep ()));
-		}
+        public TagSelectorViewController(List<ImageEntity> images) : base("TagSelectorViewController", null)
+        {
+            _images.AddRange(images.Select(x => x.CloneDeep()));
+        }
 
-		private void Initialise()
-		{
-			_tagTableSource = new TagTableSource (this);
-			_tagTokenDelegate = new TagTokenDelegate (this);
-			_tagTokenSource = new TagTokenSource (this);
-		}
+        public event EventHandler<EventArgs> Cancel = delegate { };
+        public event EventHandler<EventArgsOf<List<ImageEntity>>> Done = delegate { };
 
-		public override void ViewDidLoad ()
-		{
-			base.ViewDidLoad ();
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
 
-			Initialise ();
+            Initialise();
 
-			tagTokenView.TokenDelegate = _tagTokenDelegate;
-			tagTokenView.TokenDataSource = _tagTokenSource;
-			tagTokenView.SetupInit ();
-			tagTokenView.Layer.CornerRadius = 5;
-			tagTokenView.Layer.BorderColor = UIColor.LightTextColor.CGColor;
-			tagTokenView.Layer.BorderWidth = 1;
+            tagTokenView.TokenDelegate = _tagTokenDelegate;
+            tagTokenView.TokenDataSource = _tagTokenSource;
+            tagTokenView.SetupInit();
+            tagTokenView.Layer.CornerRadius = 5;
+            tagTokenView.Layer.BorderColor = UIColor.LightTextColor.CGColor;
+            tagTokenView.Layer.BorderWidth = 1;
 
-			tagTokenView.PlaceholderText = "Selet or Enter Tag";
-			tagTokenView.ColorScheme = new UIColor (61 / 255.0f, 149 / 255.0f, 206 / 255.0f, 1.0f);
+            tagTokenView.PlaceholderText = "Selet or Enter Tag";
+            tagTokenView.ColorScheme = new UIColor(61 / 255.0f, 149 / 255.0f, 206 / 255.0f, 1.0f);
 
-			allTags.Source = _tagTableSource;
-			btCancel.Clicked += OnCancel;
-			btDone.Clicked += OnDone;
-		}
+            allTags.Source = _tagTableSource;
+            btCancel.Clicked += OnCancel;
+            btDone.Clicked += OnDone;
+        }
 
-		private List<TagEntity> GetCommonTags()
-		{
-			List<TagEntity> result = _images.First ().Tags;
-			IEnumerable<List<TagEntity>> imageTags = _images.Select (x => x.Tags);
-			var comparer = new FuncComparer<TagEntity> ((x, y) => x.EntityId == y.EntityId);
+        private void AddTagToImages(TagEntity tag)
+        {
+            _images.Iter(x => x.AddTag(tag));
+        }
 
-			foreach (var tags in imageTags)
-			{
-				result = result.Intersect(tags, comparer).ToList();
-			}
-			return result;
-		}
+        private List<TagEntity> GetCommonTags()
+        {
+            List<TagEntity> result = _images.First().Tags;
+            IEnumerable<List<TagEntity>> imageTags = _images.Select(x => x.Tags);
+            var comparer = new FuncComparer<TagEntity>((x, y) => x.EntityId == y.EntityId);
 
-		private void AddTagToImages(TagEntity tag)
-		{
-			_images.Iter (x => x.AddTag (tag));
-		}
+            foreach (List<TagEntity> tags in imageTags)
+            {
+                result = result.Intersect(tags, comparer).ToList();
+            }
+            return result;
+        }
 
-		private void RemoveTagFormImages(TagEntity tag)
-		{
-			_images.Iter (x => x.RemoveTag (tag));
-		}
+        private void Initialise()
+        {
+            _tagTableSource = new TagTableSource(this);
+            _tagTokenDelegate = new TagTokenDelegate(this);
+            _tagTokenSource = new TagTokenSource(this);
+        }
 
-		private void OnCancel(object sender, EventArgs ea)
-		{
-			Cancel (null, EventArgs.Empty);
-			DismissViewController (true, null);
-		}
+        private void OnCancel(object sender, EventArgs ea)
+        {
+            Cancel(null, EventArgs.Empty);
+            DismissViewController(true, null);
+        }
 
-		private void OnDone(object sender, EventArgs ea)
-		{
-			var eventArgs = new EventArgsOf<List<ImageEntity>>
-			{
-				Data = _images
-			};
-			Done (null, eventArgs);
-			DismissViewController (true, null);
-		}
+        private void OnDone(object sender, EventArgs ea)
+        {
+            var eventArgs = new EventArgsOf<List<ImageEntity>>
+            {
+                Data = _images
+            };
+            Done(null, eventArgs);
+            DismissViewController(true, null);
+        }
 
-		private void ReloadData()
-		{
-			DispatchQueue.MainQueue.DispatchAsync (() => allTags.ReloadData());
-		}
+        private void ReloadData()
+        {
+            DispatchQueue.MainQueue.DispatchAsync(() => allTags.ReloadData());
+        }
 
-		private sealed class TagTableSource : UITableViewSource
-		{
-			private string cellIdentifier = "TableCell";
-			private List<TagEntity> _tags;
-			private TagSelectorViewController _controller;
+        private void RemoveTagFormImages(TagEntity tag)
+        {
+            _images.Iter(x => x.RemoveTag(tag));
+        }
 
-			public int TagCount { get { return _tags.Count; } }
 
-			public TagTableSource (TagSelectorViewController controller)
-			{
-				_controller = controller;
-				_tags = GetTags();
-			}
+        private sealed class TagTableSource : UITableViewSource
+        {
+            private const string CellIdentifier = "TableCell";
+            private readonly TagSelectorViewController _controller;
+            private List<TagEntity> _tags;
 
-			public TagEntity GetTag(int index)
-			{
-				return _tags [index];
-			}
+            public TagTableSource(TagSelectorViewController controller)
+            {
+                _controller = controller;
+                _tags = GetTags();
+            }
 
-			public void Filter (string text)
-			{
-				if (string.IsNullOrWhiteSpace (text)) 
-				{
-					_tags = GetTags();
-				}
-				else
-				{
-					_tags = GetTags()
-						.Where (x => x.Name.Contains (text))
-						.ToList ();
-					if (_tags.IsEmpty ())
-					{
-						var addTagRequest = TagEntity.AddTagRequest;
-						addTagRequest.Name = text;
-						_tags.Insert (_tags.Count, addTagRequest);
-					}
-				}
-				ReloadTags ();
-			}
+            public int TagCount
+            {
+                get { return _tags.Count; }
+            }
 
-			public override int RowsInSection (UITableView tableview, int section)
-			{
-				return _tags.Count;
-			}
+            public void Filter(string text)
+            {
+                if (string.IsNullOrWhiteSpace(text))
+                {
+                    _tags = GetTags();
+                }
+                else
+                {
+                    _tags = GetTags()
+                        .Where(x => x.Name.Contains(text))
+                        .ToList();
+                    if (_tags.IsEmpty())
+                    {
+                        TagEntity addTagRequest = TagEntity.AddTagRequest;
+                        addTagRequest.Name = text;
+                        _tags.Insert(_tags.Count, addTagRequest);
+                    }
+                }
+                ReloadTags();
+            }
 
-			public override UITableViewCell GetCell (UITableView tableView, NSIndexPath indexPath)
-			{
-				UITableViewCell cell = tableView.DequeueReusableCell (cellIdentifier);
-				if (cell == null)
-				{
-					cell = new UITableViewCell (UITableViewCellStyle.Default, cellIdentifier);
-				}
-				var tag = _tags [indexPath.Row];
-				if (tag.IsAddTagRequest)
-				{
-					cell.TextLabel.Text = string.Format("Add new tag \"{0}\"", tag.Name);
-					cell.TextLabel.TextColor = UIColor.DarkTextColor;
-				}
-				else 
-				{
-					cell.TextLabel.Text = tag.Name;
-				}
-				return cell;
-			}
+            public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+            {
+                UITableViewCell cell = tableView.DequeueReusableCell(CellIdentifier);
+                if (cell == null)
+                {
+                    cell = new UITableViewCell(UITableViewCellStyle.Default, CellIdentifier);
+                }
+                TagEntity tag = _tags[indexPath.Row];
+                if (tag.IsAddTagRequest)
+                {
+                    cell.TextLabel.Text = string.Format("Add new tag \"{0}\"", tag.Name);
+                    cell.TextLabel.TextColor = UIColor.DarkTextColor;
+                }
+                else
+                {
+                    cell.TextLabel.Text = tag.Name;
+                }
+                return cell;
+            }
 
-			public override void RowSelected (UITableView tableView, NSIndexPath indexPath)
-			{
-				TagEntity addTag;
-				TagEntity selectedTag = _tags[indexPath.Item];
-				_tags.Remove (selectedTag);
-				if (selectedTag.IsAddTagRequest)
-				{
-					_tags = GetTags ();
-					addTag = new TagEntity { Name = selectedTag.Name };
-				}
-				else
-				{
-					addTag = selectedTag;
-				}
-				_controller.AddTagToImages(addTag);
-				_controller._tagTokenSource.AddTag (addTag);
-				ReloadTags ();
-			}
+            public TagEntity GetTag(int index)
+            {
+                return _tags[index];
+            }
 
-			private List<TagEntity> GetTags()
-			{
-				var commparer = new FuncComparer<TagEntity> ((x, y) => x.Equals(y));
-				var commonTags = _controller.GetCommonTags();
-				return _tagRepository.GetUserTags()
-					.Except(commonTags, commparer)
-					.ToList();
-			}
+            public override void RowSelected(UITableView tableView, NSIndexPath indexPath)
+            {
+                TagEntity addTag;
+                TagEntity selectedTag = _tags[indexPath.Item];
+                _tags.Remove(selectedTag);
+                if (selectedTag.IsAddTagRequest)
+                {
+                    _tags = GetTags();
+                    addTag = new TagEntity { Name = selectedTag.Name };
+                }
+                else
+                {
+                    addTag = selectedTag;
+                }
+                _controller.AddTagToImages(addTag);
+                _controller._tagTokenSource.AddTag(addTag);
+                ReloadTags();
+            }
 
-			private void ReloadTags()
-			{
-				_controller.ReloadData ();
-			}
-		}
+            public override int RowsInSection(UITableView tableview, int section)
+            {
+                return _tags.Count;
+            }
 
-		private sealed class TagTokenDelegate : TokenViewDelegate
-		{
-			private readonly TagSelectorViewController _controller;
+            private List<TagEntity> GetTags()
+            {
+                var commparer = new FuncComparer<TagEntity>((x, y) => x.Equals(y));
+                List<TagEntity> commonTags = _controller.GetCommonTags();
+                return _tagRepository.GetUserTags()
+                                     .Except(commonTags, commparer)
+                                     .ToList();
+            }
 
-			public TagTokenDelegate (TagSelectorViewController controller)
-			{
-				_controller = controller;
-			}
+            private void ReloadTags()
+            {
+                _controller.ReloadData();
+            }
+        }
 
-			public override void DidDeleteTokenAtIndex (TokenView tokenView, int index)
-			{
-				_controller._tagTokenSource.RemoveTagAtIndex (index);
-				tokenView.ReloadData ();
-			}
 
-			public override void FilterToken (TokenView tokenView, string text)
-			{
-				_controller._tagTableSource.Filter (text);
-			}
-		}
+        private sealed class TagTokenDelegate : TokenViewDelegate
+        {
+            private readonly TagSelectorViewController _controller;
 
-		private sealed class TagTokenSource : TokenViewSource
-		{
-			private List<TagEntity> _source;
-			private readonly TagSelectorViewController _controller;
+            public TagTokenDelegate(TagSelectorViewController controller)
+            {
+                _controller = controller;
+            }
 
-			public TagTokenSource (TagSelectorViewController controller)
-			{
-				_controller = controller;
-				_source = controller.GetCommonTags()
-									.OrderBy(x=>x.Name).ToList();
-			}
+            public override void DidDeleteTokenAtIndex(TokenView tokenView, int index)
+            {
+                _controller._tagTokenSource.RemoveTagAtIndex(index);
+                tokenView.ReloadData();
+            }
 
-			public override string GetToken (TokenView tokenView, int index)
-			{
-				return _source[index].Name;
-			}
+            public override void FilterToken(TokenView tokenView, string text)
+            {
+                _controller._tagTableSource.Filter(text);
+            }
+        }
 
-			public override int NumberOfTokens (TokenView tokenView)
-			{
-				return _source.Count;
-			}
 
-			public void RemoveTagAtIndex(int index)
-			{
-				TagEntity tag = _source [index];
-				_controller.RemoveTagFormImages (tag);
-				_source.RemoveAt (index);
-			}
+        private sealed class TagTokenSource : TokenViewSource
+        {
+            private readonly TagSelectorViewController _controller;
+            private readonly List<TagEntity> _source;
 
-			public void AddTag(TagEntity tag)
-			{
-				_source.Add (tag);
-				_controller.tagTokenView.ReloadData ();
-			}
-		}
-	}
+            public TagTokenSource(TagSelectorViewController controller)
+            {
+                _controller = controller;
+                _source = controller.GetCommonTags()
+                                    .OrderBy(x => x.Name).ToList();
+            }
+
+            public void AddTag(TagEntity tag)
+            {
+                _source.Add(tag);
+                _controller.tagTokenView.ReloadData();
+            }
+
+            public override string GetToken(TokenView tokenView, int index)
+            {
+                return _source[index].Name;
+            }
+
+            public override int NumberOfTokens(TokenView tokenView)
+            {
+                return _source.Count;
+            }
+
+            public void RemoveTagAtIndex(int index)
+            {
+                TagEntity tag = _source[index];
+                _controller.RemoveTagFormImages(tag);
+                _source.RemoveAt(index);
+            }
+        }
+    }
 }
