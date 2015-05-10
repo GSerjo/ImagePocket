94d76423f6646595c243ef6c8997088708d58f12
GSerjo <sedot8@gmail.com>
2015-05-10 11:50:33 +0300

    * added shadow

--- Source/Dojo/ImagePreviewCell.cs
+++ Source/Dojo/ImagePreviewCell.cs
@@ -7,69 +7,6 @@ using MonoTouch.UIKit;
 
 namespace Dojo
 {
-    //    public sealed class ImagePreviewCell : UICollectionViewCell
-    //    {
-    //        private readonly ImageCache _imageCache = ImageCache.Instance;
-    //        private readonly UIImageView _imageView;
-    //		private readonly UIImageView _checkView;
-    //
-    //        [Export("initWithFrame:")]
-    //        public ImagePreviewCell(RectangleF frame) : base(frame)
-    //        {
-    //            _imageView = new UIImageView(frame)
-    //            {
-    //                Center = ContentView.Center,
-    //                ContentMode = UIViewContentMode.ScaleAspectFit,
-    //                AutoresizingMask = UIViewAutoresizing.FlexibleHeight | UIViewAutoresizing.FlexibleWidth,
-    //                ClipsToBounds = true
-    //            };
-    //
-    //			_checkView = new UIImageView (new UIImage ("Checked"));
-    //
-    //            //			_imageView.ContentMode = UIViewContentMode.ScaleAspectFill;
-    //            ContentView.AddSubview(_imageView);
-    //            ContentView.AddSubview(_checkView);
-    //        }
-    //
-    //        public override void LayoutSubviews()
-    //        {
-    //            base.LayoutSubviews();
-    ////            _imageView.Frame = Bounds;
-    //			var location = new PointF (ContentView.Bounds.Width - _checkView.Bounds.Width, 50);
-    //			_checkView.Frame = new RectangleF (location, _checkView.Frame.Size);
-    ////			checkView.Frame.Location = new PointF(ContentView.Bounds.Width - checkView.Bounds.Width, 0);
-    //        }
-    //
-    //        public void SelectCell()
-    //        {
-    //            //            BackgroundColor = UIColor.Black;
-    //            Selected = true;
-    //			_checkView.Hidden = false;
-    //        }
-    //
-    //        public void SetImage(string localId)
-    //        {
-    //            PHAsset asset = _imageCache.GetAsset(localId);
-    //            _imageCache.ImageManager.RequestImageForAsset(
-    //				asset,
-    //				_imageView.Frame.Size,
-    //				PHImageContentMode.AspectFit, null, UpdateImage);
-    //        }
-    //
-    //		private void UpdateImage(UIImage image, NSDictionary imageInfo)
-    //		{
-    //			_imageView.Image = image;
-    //		}
-    //
-    //        public void UnselectCell()
-    //        {
-    //            //            BackgroundColor = UIColor.White;
-    //            Selected = false;
-    //			_checkView.Hidden = true;
-    //        }
-    //    }
-    //}
-
     public sealed class ImagePreviewCell : UICollectionViewCell
     {
         private readonly ImageCache _imageCache = ImageCache.Instance;
@@ -87,7 +24,6 @@ namespace Dojo
                 ClipsToBounds = true
             };
             _overlayView = new UIImageView(_selectedImage);
-
             ContentView.AddSubview(_imageView);
             _overlayView.Frame = Bounds;
             ContentView.AddSubview(_overlayView);
@@ -97,9 +33,7 @@ namespace Dojo
         {
             Selected = true;
             _overlayView.Hidden = false;
-            _overlayView.Layer.ShadowColor = UIColor.Gray.CGColor;
-            _overlayView.Layer.ShadowOffset = new SizeF(0, 0);
-            _overlayView.Layer.ShadowOpacity = 0.6f;
+			_imageView.Alpha = 0.5f;
         }
 
         public void SetImage(string localId)
@@ -115,6 +49,7 @@ namespace Dojo
         {
             Selected = false;
             _overlayView.Hidden = true;
+			_imageView.Alpha = 1f;
         }
 
         private void UpdateImage(UIImage image, NSDictionary imageInfo)
