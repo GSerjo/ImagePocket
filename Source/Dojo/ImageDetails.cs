8dc9181a93bb43c06fda48f30a1d752f34593959
GSerjo <sedot8@gmail.com>
2015-05-22 23:41:11 +0300

    + ImageScrollView

---
+++ Source/Dojo/ImageDetails.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Dojo
+{
+    public class ImageDetails
+    {
+        public float Height { get; set; }
+
+        public string Name { get; set; }
+
+        public CGSize Size
+        {
+            get { return new CGSize(Width, Height); }
+        }
+
+        public float Width { get; set; }
+    }
+}
