89c64c0a5c89d2901c069ba1bdcdccb9ef0c7d3b
GSerjo <sedot8@gmail.com>
2015-06-06 18:00:23 +0300

    + PhotoViewController2

---
+++ Source/Dojo/Controllers/PhotoViewController2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Domain;
+using System.Collections.Generic;
+
+namespace Dojo
+{
+	public class PhotoViewController2
+	{
+		public PhotoViewController2(ImageEntity image, List<ImageEntity> images)
+		{
+		}
+	}
+}
+
