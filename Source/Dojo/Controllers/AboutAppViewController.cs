041ebdfb7c3bb31e06ccb08d7a111c5d1145de1d
GSerjo <sedot8@gmail.com>
2015-06-10 00:36:39 +0300

    * added twitter

--- Source/Dojo/Controllers/AboutAppViewController.cs
+++ Source/Dojo/Controllers/AboutAppViewController.cs
@@ -8,6 +8,7 @@ namespace Dojo
     public sealed class AboutAppViewController : DialogViewController
     {
         private const string AppUrl = "itms-apps://itunes.apple.com/app/id353372460";
+		private const string TwitterUrl = "https://twitter.com/ImagePocketApp";
 
         public AboutAppViewController(string caption) : base(UITableViewStyle.Grouped, null, true)
         {
@@ -19,19 +20,19 @@ namespace Dojo
 
         private static Section CreateSection()
         {
-            var result = new Section
-            {
-                new StringElement("Rate the App", OpenApp),
-                new StringElement("App Version", "1.0.1")
-            };
+			var result = new Section {
+				new StringElement ("Follow on Twitter", ()=> OpenApp(TwitterUrl)),
+				new StringElement ("Rate the App", () => OpenApp (AppUrl)),
+				new StringElement ("App Version", "1.0.1")
+			};
             return result;
         }
 
-        private static void OpenApp()
+        private static void OpenApp(string url)
         {
             try
             {
-                UIApplication.SharedApplication.OpenUrl(new NSUrl(AppUrl));
+				UIApplication.SharedApplication.OpenUrl(new NSUrl(url));
             }
             catch (Exception ex)
             {
