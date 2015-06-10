95fcd8e1f687fdf216ea78268259edcb3302f4be
GSerjo <sedot8@gmail.com>
2015-06-10 19:17:12 +0300

    * removed AboutViewController

--- Source/Dojo/Controllers/AboutAppViewController.cs
+++ Source/Dojo/Controllers/AboutAppViewController.cs
@@ -8,30 +8,31 @@ namespace Dojo
     public sealed class AboutAppViewController : DialogViewController
     {
         private const string AppUrl = "itms-apps://itunes.apple.com/app/id353372460";
-		private const string TwitterUrl = "https://twitter.com/ImagePocketApp";
+        private const string TwitterUrl = "https://twitter.com/ImagePocketApp";
 
         public AboutAppViewController(string caption) : base(UITableViewStyle.Grouped, null, true)
         {
             Root = new RootElement(caption)
             {
-				CreateAboutSection(),
+                CreateAboutSection(),
                 CreateSocialSection()
             };
         }
 
-		private static Section CreateAboutSection()
-		{
-			var result = new Section ("Test");
-			return result;
-		}
+        private static Section CreateAboutSection()
+        {
+            var result = new Section("Test");
+            return result;
+        }
 
         private static Section CreateSocialSection()
         {
-			var result = new Section {
-				new StringElement ("Follow on Twitter", ()=> OpenApp(TwitterUrl)),
-				new StringElement ("Rate the App", () => OpenApp (AppUrl)),
-				new StringElement ("App Version", "1.0.1")
-			};
+            var result = new Section
+            {
+                new StringElement("Follow on Twitter", () => OpenApp(TwitterUrl)),
+                new StringElement("Rate the App", () => OpenApp(AppUrl)),
+                new StringElement("App Version", "1.0.1")
+            };
             return result;
         }
 
@@ -39,7 +40,7 @@ namespace Dojo
         {
             try
             {
-				UIApplication.SharedApplication.OpenUrl(new NSUrl(url));
+                UIApplication.SharedApplication.OpenUrl(new NSUrl(url));
             }
             catch (Exception ex)
             {
