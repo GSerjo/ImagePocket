830f238992fb3332acaac872c290aa1557744079
GSerjo <sedot8@gmail.com>
2015-06-21 23:42:42 +0300

    + purchase

---
+++ Source/Dojo/Security/PurchaseManager.cs
@@ -0,0 +1,19 @@
+﻿using Xamarin.InAppPurchase;
+
+namespace Dojo
+{
+	public sealed class PurchaseManager
+	{
+		private static readonly PurchaseManager _instance = new PurchaseManager();
+		private readonly InAppPurchaseManager _purchaseManager = new InAppPurchaseManager();
+
+		private PurchaseManager ()
+		{
+		}
+
+		public static PurchaseManager Instance
+		{
+			get { return _instance; }
+		}
+	}
+}
