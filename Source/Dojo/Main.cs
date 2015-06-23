5bc88d283478e8b5b0d5212f43582f31731f4766
GSerjo <sedot8@gmail.com>
2015-06-23 22:40:53 +0300

    + Insights

--- Source/Dojo/Main.cs
+++ Source/Dojo/Main.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using UIKit;
+using Xamarin;
 
 namespace Dojo
 {
@@ -10,6 +11,14 @@ namespace Dojo
         {
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
+            Insights.HasPendingCrashReport += (sender, isStartupCrash) =>
+            {
+                if (isStartupCrash)
+                {
+                    Insights.PurgePendingCrashReports().Wait();
+                }
+            };
+            Insights.Initialize("eeb71246a815e0d405a338047f5a308003a4514d");
             UIApplication.Main(args, null, "AppDelegate");
         }
     }
